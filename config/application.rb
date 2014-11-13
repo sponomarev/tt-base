120d4237566f4763feeee2a34c1461bfeffe1693
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-11-14 00:45:04 +0300

    Add basic application settings

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,16 +14,17 @@ Bundler.require(*Rails.groups)
 
 module TtBase
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Moscow'
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    # Application locale
+    I18n.enforce_available_locales = true
+    I18n.default_locale = :ru
+    I18n.locale = :ru
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    # Disable unnecessary generators
+    config.generators do |g|
+      g.assets = false
+      g.helper = false
+    end
   end
 end
