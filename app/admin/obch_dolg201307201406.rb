adb4514d00aa5da2908dc427846f6b0eb3a70657
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-11-17 02:55:28 +0300

    Add admin area for depts models

---
+++ app/admin/obch_dolg201307201406.rb
@@ -0,0 +1,43 @@
+ActiveAdmin.register ObchDolg201307201406 do
+  actions :all, except: [:new, :edit, :destroy]
+
+  filter :account
+  filter :street
+  filter :house
+  filter :appartment
+  filter :lastname
+  filter :firstname
+  filter :middlename
+  filter :period
+  filter :service
+  filter :opening_balance
+  filter :earn
+  filter :benefit
+  filter :recalculation
+  filter :compensation
+  filter :payable
+  filter :payment
+  filter :outgoing_balance
+
+  index do
+    selectable_column
+    column :account
+    column :street
+    column :house
+    column :appartment
+    column :lastname
+    column :firstname
+    column :middlename
+    column :period
+    column :service
+    column :opening_balance
+    column :earn
+    column :benefit
+    column :recalculation
+    column :compensation
+    column :payable
+    column :payment
+    column :outgoing_balance
+    actions
+  end
+end
