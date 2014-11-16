d658aa04d3c7c2064a24f8c3473b53c8c28ac19c
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-11-17 02:46:58 +0300

    Change active_admin settings to leave csv only

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -8,6 +8,7 @@ ActiveAdmin.setup do |config|
   config.root_to = 'dashboard#index'
   config.allow_comments = false
   config.batch_actions = true
+  config.download_links = [:csv]
 
   # == CSV options
   #
@@ -17,39 +18,10 @@ ActiveAdmin.setup do |config|
   # Force the use of quotes
   # config.csv_options = { :force_quotes => true }
 
-  # == Download Links
-  #
-  # You can disable download links on resource listing pages,
-  # or customize the formats shown per namespace/globally
-  #
-  # To disable/customize for the :admin namespace:
-  #
-  #   config.namespace :admin do |admin|
-  #
-  #     # Disable the links entirely
-  #     admin.download_links = false
-  #
-  #     # Only show XML & PDF options
-  #     admin.download_links = [:xml, :pdf]
-  #
-  #     # Enable/disable the links based on block
-  #     #   (for example, with cancan)
-  #     admin.download_links = proc { can?(:view_download_links) }
-  #
-  #   end
-
   # == Pagination
   #
   # Pagination is enabled by default for all resources.
   # You can control the default per page count for all resources here.
   #
   # config.default_per_page = 30
-
-  # == Filters
-  #
-  # By default the index screen includes a “Filters” sidebar on the right
-  # hand side with a filter for each attribute of the registered model.
-  # You can enable or disable them for all resources here.
-  #
-  # config.filters = true
 end
