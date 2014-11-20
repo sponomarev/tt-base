896bc7c805d2068740af4cc54dd38d66cbcb3e33
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-11-21 01:31:10 +0300

    Change csv separator

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -16,7 +16,7 @@ ActiveAdmin.setup do |config|
   config.csv_options = { col_sep: ';' }
   #
   # Force the use of quotes
-  config.csv_options = { force_quotes: true }
+  # config.csv_options = { force_quotes: false }
 
   # == Pagination
   #
