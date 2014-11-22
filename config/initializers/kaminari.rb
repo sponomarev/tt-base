448212bca05836d50d99e95e79db1499ccec8f8f
Sergey Ponomarev <sergey.ponomarev@saltpepper.ru>
2014-11-22 22:27:51 +0300

    Revert "Add Kaminari count hack to speed up queries"
    This reverts commit 0526b65179526cfd4f6fa4c088c1f3f71c50b96a.

--- config/initializers/kaminari.rb
+++
@@ -1,7 +0,0 @@
-module Kaminari
-  module PageScopeMethods
-    def total_count
-      @_hacked_total_count || (@_hacked_total_count = self.connection.execute("SELECT (reltuples)::integer FROM pg_class r WHERE relkind = 'r' AND relname ='#{table_name}'").first["reltuples"].to_i)
-    end
-  end
-end
